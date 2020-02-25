5de112fda93f21dc7efa90d53cbe93be80369f1b
Federico <federico.caiello@gmail.com>
2020-02-25 23:21:16 +0100

    add scss

--- dist/app.js
+++ dist/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Fede\Desktop\MAMP.public\php-ajax-dischi\src\app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Fede\Desktop\MAMP.public\php-ajax-dischi\src\app.scss */"./src/app.scss");
+__webpack_require__(/*! C:\Users\Fede\desktop\MAMP.public\php-ajax-dischi\src\app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Fede\desktop\MAMP.public\php-ajax-dischi\src\app.scss */"./src/app.scss");
 
 
 /***/ })
--- index.php
+++ index.php
@@ -7,18 +7,33 @@
  <html lang="en" dir="ltr">
    <head>
      <meta charset="utf-8">
+     <link rel="stylesheet" href="dist/app.css">
      <title></title>
    </head>
    <body>
-     <ul>
-       <?php foreach ($database as $cd) { ?>
-         <li>
+     <header>
+       <div class="container">
+           <img src="logo.png" alt="logo">
+           <select class="selezione" name="generi">
+             <option value="tutto">Tutti i generi</option>
+             <option value="jazz">Jazz</option>
+             <option value="pop">Pop</option>
+             <option value="rock">Rock</option>
+             <option value="metal">Metal</option>
+           </select>
+       </div>
+     </header>
+     <div class="cds-container container">
+         <?php foreach ($database as $cd) { ?>
+           <div class="cd">
            <img src="<?php echo $cd["poster"];?>" alt="">
-           <p><?php echo $cd["title"];?></p>
-           <p><?php echo $cd["author"];?></p>
-           <p><?php echo $cd["year"];?></p>
-         </li>
-       <?php } ?>
-     </ul>
+           <h3><?php echo $cd["title"];?></h3>
+           <span class="author"><?php echo $cd["author"];?></span>
+           <span class="year"><?php echo $cd["year"];?></span>
+         </div>
+         <?php } ?>
+     </div>
+
+
    </body>
  </html>
